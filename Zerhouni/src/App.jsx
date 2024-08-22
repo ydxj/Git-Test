b8e3422b9c858c21b095dae354019f328d32805b
Zerhouni Omar <hddjdndjdndj@gmail.com>
2024-08-22 17:42:13 +0100

    Version 2 adding default prop

--- Zerhouni/src/App.jsx
+++ Zerhouni/src/App.jsx
@@ -1,13 +1,12 @@
-import Card from './Card'
-import './App.css'
+import Card from './Card.jsx'
+
 
 function App() {
 
   return (
     <>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card name="Zerhouni Omar" bio="Hello my name is omar zerhouni i'm full stuck dev"/>
+      <Card name="Oumaima Hassouni" bio="Hello my name is Oumaima Hassouni i'm full stuck dev"/>
       <Card/>
       <Card/>
     </>
--- Zerhouni/src/Card.jsx
+++ Zerhouni/src/Card.jsx
@@ -1,11 +1,50 @@
 import ProfilPic from './assets/profile.png'
-import './App.css'
-export default function Card(){
+import PropType from 'prop-types'
+function Card(prop){
+    const CardStyle = {
+        margin: "20px",
+        border: "1px white solid",
+        padding: "10px",
+        boxShadow: "1px 1px 10px rgb(23, 156, 156)",
+        width: "200px",
+        height: "250px",
+        display: "inline-block",
+        textAlign: "center",
+    
+    }
+    const ImageStyle = {
+        borderRadius:"50%" ,
+        marginBottom: "10px",
+        height: "100px",
+        width: "auto",
+    
+    }
+    const TitleStyle = {
+        margin: "0",
+        padding: "0",
+        fontFamily: "'Courier New', Courier, monospace",
+        color: "rgb(243, 162, 162)",
+    }
+    const Paragraph ={
+        fontFamily: "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif",
+        color: "rgb(6, 33, 243)",
+    }
     return(
-        <div className='card'>
-            <img src={ProfilPic} alt="Profile"/>
-            <h2>Zerhouni Omar</h2>
-            <p>Hello my name is Omar Zerhouni and i'm a Full Stuck Devloper</p>
+        <div style={CardStyle}>
+            <img src={ProfilPic} alt="Profile" style={ImageStyle}/>
+            <h2 style={TitleStyle}>{prop.name}</h2>
+            <p style={Paragraph}>{prop.bio}</p>
         </div>
     );
 }
+// hna radi ndiro dok les prop li kayverifiw wach inputs dyalna shah wlala
+Card.propTypes = {
+    name : PropType.string,
+    bio : PropType.string,
+}
+// hna kanzido dok les valeurs li bghinahom ykono par default
+Card.defaultProps = {
+    name : "Guest",
+    bio : "Hello world i don't have any bio"
+}
+export default Card
